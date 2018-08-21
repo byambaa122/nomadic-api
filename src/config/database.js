c14cd59c6496d41af551dc7b26725a9e47b457f3
unknown <byambaa.122@gmail.com>
2018-08-22 00:15:37 +0800

    comment refactor

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose'
 
-/**
- * Mongo DB connection URI
- * 
- * @var {String} uri
- */
+// Database connection
 const uri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
 mongoose.connect(uri, {
